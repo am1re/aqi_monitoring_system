a1dd6169e26975a1eb1560c355b6ab3864082c5f
Amir Abu Shamala <amir_abu_shamala@epam.com>
2020-05-26 13:34:49 +0300

    replication, minor fixes

--- scraper/pipelines.py
+++ scraper/pipelines.py
@@ -5,6 +5,6 @@ class MongoPipeLine:
     def __init__(self):
         self.db = get_collection()
 
-    def process_item(self, item):
+    def process_item(self, item, spider):
         print(item)
         self.db.insert_one(item)
--- src/database.py
+++ src/database.py
@@ -4,8 +4,13 @@ import csv
 import pymongo
 from dotenv import load_dotenv
 
-
 load_dotenv()
+if os.getenv('MONGO_URL') is None:
+    try:
+        url = open('db_url.txt', 'r').read().replace('\n', '')
+    except FileNotFoundError as e:
+        url = "localhost:27017"
+    os.environ["MONGO_URL"] = url
 
 
 def get_collection():
